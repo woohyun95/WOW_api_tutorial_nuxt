a2ea69e82434b648705c5f5f2a19ed0cc0273377
woohyun95 <diddngus77@naver.com>
2020-12-02 17:14:03 +0900

    account ID

--- store/apiAccount.js
+++ store/apiAccount.js
@@ -1,5 +1,4 @@
 export default {
-  clientID: '9691cab201a64a55a363e93a70101f3f',
-  clientSecret : 'z2lEfgtXze8F1Dp6iMZGAmv4gYv6cgKm',
-  characterProfile:{}
+  clientID: 'input your clientID',
+  clientSecret : 'input your clientSecret'
 }
